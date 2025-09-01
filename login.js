965f15c5b45a8a416ef99318661d5b90b122a04a
Gnmoreira <107262839+Gnmoreira@users.noreply.github.com>
2025-09-01 12:16:58 +0000

    exemplo

--- login.js
+++ login.js
@@ -1,16 +1,17 @@
-const email ="admin@email.com";
+const email = "admin@email.com";
 const senha = "admin";
 
-function verificarLogin(){
-  const emailInformado = document.getElementById("email").value;   
-  const senhaInformada = document.getElementById("senha").value;   
-  if(emailInformado === email){
-alert("email informado corretamente");
-if(senhaInformada=== senha){
-alert("senha informada corretamente");
-window.location ="home.html";
-} 
-alert("senha incorreta!"); } else {
-    alert("email incorreto");
-  }
+function verificarLogin() {
+    const emailInformado = document.getElementById("email").value;
+    const senhaInformada = document.getElementById("senha").value;
+
+    if (emailInformado === email) {
+        alert("E-mail informado corretamente!");
+        if (senhaInformada === senha) {
+            alert("Senha informada corretamente!");
+            window.location = "home.html";
+        } else
+            alert("Senha incorreta!");
+    } else
+        alert("E-mail incorreto!");
 }
