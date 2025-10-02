36294a183529982c90c9b31e627b2bd78e280f18
Gnmoreira <107262839+Gnmoreira@users.noreply.github.com>
2025-10-02 11:35:07 +0000

    exemplo

--- login.js
+++ login.js
@@ -1,14 +1,14 @@
-const email = "admin@email.com";
-const senha = "admin";
+const email = usuario.email;
+const senha = usuario.senha;
 
 function verificarLogin() {
     const emailInformado = document.getElementById("email").value;
     const senhaInformada = document.getElementById("senha").value;
 
     if (emailInformado === email) {
-        alert("E-mail informado corretamente!");
+        //alert("E-mail informado corretamente!");
         if (senhaInformada === senha) {
-            alert("Senha informada corretamente!");
+            //alert("Senha informada corretamente!");
             window.location = "home.html";
         } else
             alert("Senha incorreta!");
